5f11c50e4980b0b3c53296b92fdd494f9ed102c1
Francisco Pinto <frpinto@fc.ul.pt>
2019-03-08 23:55:13 +0000

    correction of specific_neighbors function

--- specific_neighbors.R
+++ specific_neighbors.R
@@ -1,16 +1,14 @@
 specific_neighbors=function(g,s){
   
-  bigadj=as_adjacency_matrix(g,names=F,sparse=F)
+  bigadj=as_adjacency_matrix(g,names=F)
   neib=rowSums(as.matrix(bigadj[,s]))
-  
-  
-  multineibtab$neib=neib #3
-  multineibtab$degree=degree(g) #9
+  gdegree=rowSums(as.matrix(bigadj[,]))
+  multineibtab=data.frame(id=(1:length(neib)),neib=neib,degree=gdegree,stringsAsFactors=F)
   n1=length(s)
   ntot=vcount(g)-1
   
   multineibtab$p=-log10(phyper(multineibtab$neib,n1,ntot-n1,multineibtab$degree,lower.tail=F))
-  multineibtab$r=rank(multineibtab$p_wt)/vcount(g)
+  multineibtab$r=rank(multineibtab$p)/vcount(g)
   multineibtab
 }
 
